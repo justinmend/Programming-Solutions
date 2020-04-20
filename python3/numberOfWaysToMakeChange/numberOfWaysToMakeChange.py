6afe776cbd68f8fc7dad26d6974fbe2890447dea
Justin Mendiguarin <justinmendiguarin@Justins-MBP-2.attlocal.net>
2020-04-20 02:42:34 -0700

    update solution in python3 for problem numberOfWaysToMakeChange using dynamic programming

--- python3/numberOfWaysToMakeChange/numberOfWaysToMakeChange.py
+++ python3/numberOfWaysToMakeChange/numberOfWaysToMakeChange.py
@@ -7,43 +7,37 @@
 
   Note that an unlimited amount of coins is at your disposal.
 '''
-# Time - O (D * N) | Space - O(N)
-# D represents the length of the denoms list.
-# N represents the value of the n input.
-# For every denom in the denoms list, we iterate i up to the range of n where we try to
-# determine if we can make change with the current denom value out of the current target i.
-# The size ouf our dp list is proportionate to the input value n.
+# Time - O(D * N) | Space - O(N)
+# D represents the length of denoms list
+# N represents the integer value of n
 
 
 def numberOfWaysToMakeChange(n, denoms):
     # Assumptions:
+    # 1. Does not using a denom to make change out of an amount count as a way to make change? Yes.
+
     # Notes:
-    # 1. Given an array of positive integers. denoms[i] > 0
+    # 1. Given an array of positive integers representing coin denominations. denoms[i] > 0
     # 2. Given a single non-negative integer n representing a target amount of money. n >= 0
+    # 3. Return the number of ways to make change for the target amount
 
     # Brute Force:
-    # Create a dp list to store previous calculations
-    # Initialize dp with values of 0 and length of n + 1.
-    # Add 1 to n to handle zero-based index
-    dp = [0] * (n + 1)
 
-    # Initialize dp[0] to 1, so that when we reference the dp value for the difference between target amount and denom,
-    # where it happens to be a perfect exact change, we will get a value of 1 instead of 0 from dp[0].
-    # A perfect exact change with a denom to the target value should count as a way to make change.
+    # PSEUDOCODE:
+    # Iterate through the denoms:
+    # Iterate up through n:
+    # If we can make change out of the curent amount with our current denom,
+    # populate current dp value.
+    # Make sure to add to the current dp value the complementary changes' dp value (other ways to make change)
+    # return dp[n]
+
+    dp = [0] * (n+1)
+    # Not using a denom counts as a way to make a change
     dp[0] = 1
 
-    # Iterate through the denoms:
     for denom in denoms:
-        # For every current target amount populate dp list
-        # if we can make change out of it with the current denom.
-        for targetAmount in range(n + 1):
-            # Make sure to only consider current target amount if they are
-            # greater than or equal to the current denom. If the current target amount is less
-            # than the denom then we know we can't make a change out of it.
-            if targetAmount >= denom:
-                # Get the cached dp value of the complement amount
-                # to the current denom value necessary to make a complete change out of the current target amount.
-                complementAmount = targetAmount - denom
-                dp[targetAmount] = dp[targetAmount] + dp[complementAmount]
+        for amount in range(n + 1):
+            if amount >= denom:
+                dp[amount] = dp[amount] + dp[amount-denom]
 
     return dp[n]
